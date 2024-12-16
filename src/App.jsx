0c1431e108d95bf149e7559d626a6e32a58f26dc
Usama <uelareeny@gmail.com>
2024-12-17 00:26:10 +0200

    Added the WishList Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import Header from "./components/Header/Header.jsx";
+import WishList from "./components/WishList/WishList.jsx";
 import Categories from "./components/Categories/Categories.jsx";
 import Books from "./components/Books/Books.jsx";
 import Footer from "./components/Footer/Footer.jsx";
-import BookView from "./components/BookView/BookView.jsx";
 export default function App() {
   return (
     <>
       <Header />
-      <BookView />
-      {/* <section className="flex flex-col md:flex-row my-8">
+      <section className="flex flex-col md:flex-row my-8">
         <Categories />
         <Books />
-      </section> */}
+      </section>
       <Footer />
     </>
   );
